331e341274216b251311e3c918a34e6f3e55bb92
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2024-12-12 15:36:02 +0100

    Create Input-Name.py

---
+++ Input-Name.py
@@ -0,0 +1,8 @@
+# first exercise
+firstname = input("firstname ?" )
+lastname = input("lastname? ")
+print(firstname  + " " + lastname)
+
+print(f"firstname: {firstname} lastname: {lastname}")
+
+# second exercise

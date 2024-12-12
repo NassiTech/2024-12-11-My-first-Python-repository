6af6e781942d27618c4685c251b73e73769a5808
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2024-12-12 15:10:51 +0100

    my

---
+++ 2024-12-12-morning-exercise.py
@@ -0,0 +1,22 @@
+
+
+# first version with string data type 
+
+person1 = "Thomas"
+age_person1 = 35
+person2 = "Marc"
+age_person2 = 27
+age_person1 = input("give the person1 age:")
+print(f"The type of give the person1 age: {type(age_person1)}")
+if int(age_person1) !=35: "FALSE", print("FALSE")
+age_person2 = input("give the person2 age:")
+print(f"The type of give the person2 age: {type(age_person2)}")
+if int(age_person2) != 27: "FALSE", print("FALSE")
+
+print(int(age_person1)+int(age_person2))
+print(age_person1 + age_person2)
+
+
+# separate string and integer
+# age_person1_int = int(age_person1)
+# age_person2_int = int(age_person2)
---
+++ simpleOperations.py
@@ -0,0 +1,14 @@
+
+
+first_name = "Nassima"
+last_name = "A."
+print(first_name + " " + last_name)
+
+
+number1 = 3
+number2 = 15
+print("number1+ number2", number1 + number2)
+
+print("number1 * number2" , number1 * number2 )   
+
+print("number2/number1" , number2 / number1)

c45e0d1dfe21b1780176ef79d0a6f55fb1c699af
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2024-12-12 15:58:13 +0100

    ex

--- exercises1-2-3.py
+++ exercises1-2-3.py
@@ -9,6 +9,7 @@ print(f"firstname: {firstname} lastname: {lastname}")
 first_number = int(input("first number ?" ))
 second_number = int(input("second number? "))
 print(f"the summ is:  {first_number + second_number}")
+
 # If condition
 first_number = int(input("number ? "))
 if first_number >= 0: 

66bad657de0416576a1ae4f3f6f4543749445d56
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2024-12-13 12:29:48 +0100

    20

--- exercises1-2-3.py
+++ exercises1-2-3.py
@@ -1,18 +1,18 @@
 # first exercise
-firstname = input("firstname ?" )
+firstname = input("firstname ?")
 lastname = input("lastname? ")
-print(firstname  + " " + lastname)
+print(firstname + " " + lastname)
 
 print(f"firstname: {firstname} lastname: {lastname}")
 
-# second exercise : add numbers 
-first_number = int(input("first number ?" ))
+# second exercise : add numbers
+first_number = int(input("first number ?"))
 second_number = int(input("second number? "))
 print(f"the summ is:  {first_number + second_number}")
 
 # If condition
 first_number = int(input("number ? "))
-if first_number >= 0: 
+if first_number >= 0:
     print("positive number or zero")
 else:
-    print("negative number") 
+    print("negative number")

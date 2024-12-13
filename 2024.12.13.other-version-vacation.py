0488b34c482b29f4a832eacb917578cfb6789192
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2024-12-13 15:38:50 +0100

    2024

---
+++ 2024.12.13.other-version-vacation.py
@@ -0,0 +1,20 @@
+# vacation calendar more elegant version!!
+
+d = input("enter day of the year:")
+
+if d >= "2024.12.24" and d <= "2025.01.02":
+    print(f"This day belongs to Christmas vacation '24 : {d:}")
+else:
+    if d >= "2024.04.18" and d <= "2024.04.21":
+        print(f"This day belongs to Easter vacation : {d}")
+    else:
+        if d > "2024.08.08" and d <= "2024.08.19":
+            print(f"This day belongs to summer vacation : {d}")
+        else:
+            if d >= "2025.12.24" and d <= "2026.01.02":
+                print(f"This day belongs to Christmas vacation '25 : {d}")
+            else:
+                if d >= "2025.07.14" and d <= "2025.07.25":
+                    print("this is my extra private vacation ;-P")
+                else:
+                    print("It is a working day :-( ")

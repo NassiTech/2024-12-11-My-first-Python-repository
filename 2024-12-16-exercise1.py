d08d99d7501648411fc53b1f30b0b8a96475710e
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2024-12-16 17:20:26 +0100

    20

--- 2024-12-16-exercise1.py
+++ 2024-12-16-exercise1.py
@@ -1,25 +1,39 @@
-def what_is_your_age():
-    age
+# define a function that asks you for your age /name
+# display on the screen the confirmation of the answer
 
 
-age = int(input(f"enter your age: "))
-print(age)
-exit()
+# def what_is_your_age():
+#    age
+
+
+# age = int(input(f"enter your age: "))
+# print("your age is :{age}:")
 
 
 def what_is_your_name():
-    name = input(f"what_is_your_name: {whatisyourname : }")
-    print("what is your name")
-    # Lukas version
-    # def namenabfrage()
-    # name = input("gib deinen  Namen an : ")
-    # print("Du bist also {name}.")
+
+    name = input(f"what_is_your_name ?: name :")
+
+    #    print("what is your name?:")
+    return name
+    print("hallo")
+
+
+### the command print doesnt mean anything after return : because return terminates the executuion of the first part!!!
+####### here is still th definition ###
+
+# print( what_is_your_name())
+name2 = what_is_your_name()
+print(name2)
+
+
+# Lukas version
+# def namenabfrage()
+# name = input("gib deinen  Namen an : ")
+#   print("Du bist also {name}.")
 
 
 # mit return
-def namenabfrage2():
-    name = input("gib denene Namen an:")
-    return f"du bist also {name}."
-
-
-exit()
+# def namenabfrage2():
+# name = input("gib denene Namen an:")
+# return f"du bist also {name}."

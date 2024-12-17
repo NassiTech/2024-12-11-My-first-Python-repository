24b705bdfd4dda7758b8f37a8e41575e2f2df536
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2024-12-17 11:33:08 +0100

    20

---
+++ 2024-12-17-class-convert.py
@@ -0,0 +1,15 @@
+# calculate area rectangle ex: calc_area(width,height)
+# write a function miles_kilometrs (m) or kilometrs_miles
+# write a function conversion celcius_Farenheit or Farenheit_celcius
+def area_rectangle(w, l):
+    return w * l
+
+
+# w = width and l = length
+
+w = float(input(f"enter width :"))
+l = float(input(f"enter length :"))
+
+
+area_rectangle(w, l)
+print("area of rectangle")
